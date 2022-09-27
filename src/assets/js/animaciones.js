21ac54d45b018702136d83e5e92ff4da8572831e
Lean Alonso <lea.alonso99@gmail.com>
2022-09-27 16:25:06 -0300

    Agregando init con deploy firebase, edicion en js

--- src/assets/js/animaciones.js
+++ src/assets/js/animaciones.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function(){
     // let animado = this.document.getElementsByClassName('animado');
     let animado = document.getElementById('skill');
     let posicion1 = animado.getBoundingClientRect().top;
-    console.log(posicion1);
+    // console.log(posicion1);
     let tamanioPantalla = window.innerHeight/2;
 
     if(posicion1 < tamanioPantalla){
